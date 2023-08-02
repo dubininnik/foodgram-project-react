b1c5b39f828106a4e9d07eaeba35575cac1d0283
Nikolay Dubinin <dubinin.kerch@yandex.ru>
2023-08-02 17:12:14 +0300

    Minor fix in subscribe validation

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -95,11 +95,11 @@ class SubscribeAuthorSerializer(serializers.ModelSerializer):
                   'last_name', 'recipes', 'recipes_count')
 
     def validate(self, obj):
-        user = obj.get('user')
-        author = obj.get('author')
-        if user == author:
+        request = self.context['request']
+        author = obj.get['author']
+        if request.user == author:
             raise serializers.ValidationError('Нельзя подписаться на себя')
-        if obj.subscriber.filter(user=user).exists():
+        if obj.subscriber.filter(user=request.user).exists():
             raise serializers.ValidationError('Подписка уже оформлена')
         return obj
 
